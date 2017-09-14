9362c21b6a21f7d5e74a00691fbf26e0b27d9e37
Lumengtao <394125120@qq.com>
2017-09-14 15:08:19 +0800

    test
    regularization

--- new_pro/test_re.py
+++ new_pro/test_re.py
@@ -1,7 +1,5 @@
 import re
 
-import re
-
 pattern = re.compile(r'(.*)(\d{4}/\d{1,2}/\d{1,2})')
 
 #match = pattern.match('hello 2016/8/12 111/23/4')
@@ -12,3 +10,4 @@ if match:
 
 s='12 34 st'
 print(s.split())
+
